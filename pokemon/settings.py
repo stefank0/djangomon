b60708a92ac4cbf0f1e80afed8925b7536eae3a8
stefank0 <stefankorenberg@hotmail.com>
2020-02-29 13:16:26 +0100

    Add app to settings.

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'battle_simulator.apps.BattleSimulatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
