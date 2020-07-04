d84e1b850ece69387b08e505773ed1007e7a4a61
Nick-python <nickfish90@gmail.com>
2020-07-04 18:58:35 +0200

    Damage calculation + accuracy determination

--- pokemon/models/move.py
+++ pokemon/models/move.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 from django.db import models
 
 from .type import Type
@@ -22,27 +25,35 @@ class Move(models.Model):
     def __str__(self):
         return self.name
 
-    def stab(self, attacker):
+    def stab(self, attacker, damage):
         """Same type attack bonus (STAB)."""
         types = [attacker.species.type1, attacker.species.type2]
-        return 1.5 if (self.type in types) else 1.0
+        return (3 * damage) // 2 if (self.type in types) else damage
 
     def damage_expected(self, attacker, defender):
         """Expectation value of the damage."""
-        return self.damage_value(attacker, defender) * self.accuracy
+        damage = self.stab(attacker, self.damage_value(attacker, defender))
+        effectiveness = defender.species.effectiveness(self.type)
+        return math.floor(effectiveness * damage * 0.925 * self.accuracy)
 
     def damage_value(self, attacker, defender):
         """Damage when the attacker hits the defender."""
         if self.damage_class == 'PH':
-            stat_factor = attacker.attack / defender.defense
+            attack_stat = attacker.attack
+            defense_stat = defender.defense
         else:
-            stat_factor = attacker.special_attack / defender.special_defense
-        level_factor = 2.0 * attacker.level / 5.0 + 2.0
-        base_damage = (self.power * level_factor * stat_factor) / 50.0 + 2.0
-        stab = self.stab(attacker)
-        effectiveness = defender.species.effectiveness(self.type)
-        return base_damage * stab * effectiveness
+            attack_stat = attacker.special_attack
+            defense_stat = defender.special_defense
+        level_factor = (2 * attacker.level) // 5 + 2
+        return (level_factor * self.power * attack_stat // defense_stat) // 50 + 2
 
     def damage(self, attacker, defender):
         """Actual damage when used by an attacker against a defender."""
-        return self.damage_value(attacker, defender)                            # TODO: Use accuracy and randomness.
+        if random.random() * 100 < self.accuracy:
+            damage = self.damage_value(attacker, defender)
+            damage *= random.randrange(85, 101) // 100
+            damage = self.stab(attacker, damage)
+            effectiveness = defender.species.effectiveness(self.type)
+            return math.floor(effectiveness * damage)
+        else:
+            return 0
