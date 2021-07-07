7347591e3b97959b9bedbd37362f267f5eb98a2e
stefank0 <stefankorenberg@hotmail.com>
2021-07-07 12:49:40 +0200

    Resume functionality

--- pokemon/simulator.py
+++ pokemon/simulator.py
@@ -2,11 +2,17 @@ from pokemon.models import BattleLog
 from pokemon.models import Pokemon
 
 
-def battle():
+def battle(resume_pokemon1=None, resume_pokemon2=None):
     """Run the Pokemon battle simulator."""
+    skip = resume_pokemon1 and resume_pokemon2
     pokemons = Pokemon.objects.order_by('id')
     for pokemon1 in pokemons:
         for pokemon2 in pokemons:
+            if skip:
+                if (pokemon1 == resume_pokemon1) and (pokemon2 == resume_pokemon2):
+                    skip = False
+                else:
+                    continue
             if pokemon1 != pokemon2:
                 winner, report = pokemon1.battle(pokemon2)
                 loser = pokemon1 if winner is pokemon2 else pokemon2
